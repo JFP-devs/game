2c85f514afeb0b1d08e2f4c3445e162f04d66ead
RiadMefti <riadmefti@hotmail.com>
2024-06-28 15:30:36 -0400

    adding beautiful weapon

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -2,7 +2,11 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+var current_weapon: Node = null
 
+func _ready():
+	# Equip default weapon
+	equip_weapon(preload("res://Scene/DefaultWeapon.tscn"))
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO # The player's movement vector.
@@ -18,3 +22,13 @@ func _physics_process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	move_and_slide()
+
+
+
+func equip_weapon(weapon_scene):
+	if current_weapon:
+		current_weapon.queue_free()
+	
+	current_weapon = weapon_scene.instantiate()
+	$WeaponSlot.add_child(current_weapon)
+
