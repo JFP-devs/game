0949033a0583628ac285f71c4931590def49a955
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-06-28 22:33:45 -0400

    Added spawn enemy and finds closest player

---
+++ Scene/Enemy_movement.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 350.0
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+func _find_closest_player(players):
+	var astar = AStar2D.new()
+	for i in range(len(players)):
+		astar.add_point(i, players[i].position)
+	var id = astar.get_closest_point(self.position)
+	return players[id]
+
+func _physics_process(delta):
+	var players = get_tree().get_nodes_in_group("player")
+	var closest_player = _find_closest_player(players)
+	print(closest_player)
+	move_and_slide()
--- Scripts/spawn.gd
+++ Scripts/spawn.gd
@@ -1,16 +1,22 @@
 extends StaticBody2D
 
-
+var enemy_scene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var timer = $Timer
 	timer.connect("timeout", self._on_Timer_timeout)
 	timer.wait_time = 5.0
 	timer.start()
+	enemy_scene = preload("res://Scene/Enemy.tscn")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_Timer_timeout():
 	print("Timeout")
+	var enemy = enemy_scene.instantiate()
+	var mob_spawn_location = $Path2D/PathFollow2D
+	mob_spawn_location.progress_ratio = randf()
+	enemy.position = mob_spawn_location.position
+	self.add_child(enemy)
 	$Timer.start()
